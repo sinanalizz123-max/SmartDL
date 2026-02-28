783fe7ba0a2b7caf8550929d5dedcaa8feedaa95
sinanali <sinanalizz123@gmail.com>
2026-02-28 18:20:50 +0530

    Use debug signing when release keystore missing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,11 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = if (keystorePropsFile.exists()) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
